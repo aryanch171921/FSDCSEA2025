b43f0faa9e848d8bafcebe27da302c7377c026e3
aryanch171921 <aryanch171921@gmail.com>
2026-02-13 13:18:45 +0530

    assignment -1

---
+++ fsd6thsem/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div style={{ textAlign: "center" }}>
+        <h1>ABES ENG</h1>
+        <h2>ABES College</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ fsd6thsem/frontend/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">ABES</div>
+
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>Work</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
